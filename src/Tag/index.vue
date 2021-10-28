d8efe76150bc2e487bc8c5b0d53100becc55f30d
Peter roe <peterroe@163.com>
2021-10-28 14:21:41 +0800

    :tada: first component

---
+++ src/Tag/index.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Descripttion: 
+ * @Author: 林舒恒
+ * @Date: 2021-10-28 14:09:20
+ * @LastEditors: 林舒恒
+ * @LastEditTime: 2021-10-28 14:20:27
+-->
+<script setup>
+    const { slots, attrs } = useContext()
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
