f3a3a2cc748739ab5ebaa886aed3f02c3777dc60
Peter roe <peterroe@163.com>
2021-11-11 14:17:28 +0800

    :bug: fix: 修复文档Icon的布局，改用grid

--- src/icon/util/icons.vue
+++ src/icon/util/icons.vue
@@ -1,121 +1,112 @@
 <template>
     <div class="u-icons" ref="icons">
-        <Generate :type="IconActivity" copy-text="<IconActivity />" />
-        <Generate :type="IconAlert" copy-text="<IconAlert />" />
-        <Generate :type="IconArchive" copy-text="<IconArchive />" />
-        <Generate :type="IconArrowBottom" copy-text="<IconArrowBottom />" />
-        <Generate :type="IconArrowLeft" copy-text="<IconArrowLeft />" />
-        <Generate :type="IconArrowRight" copy-text="<IconArrowRight />" />
-        <Generate :type="IconArrowTop" copy-text="<IconArrowTop />" />
-        <Generate :type="IconBackwards" copy-text="<IconBackwards />" />
-        <Generate :type="IconBag" copy-text="<IconBag />" />
-        <Generate :type="IconBan" copy-text="<IconBan />" />
-        <Generate :type="IconBell" copy-text="<IconBell />" />
-        <Generate :type="IconBook" copy-text="<IconBook />" />
-        <Generate :type="IconBookmark" copy-text="<IconBookmark />" />
-        <Generate :type="IconCalendar" copy-text="<IconCalendar />" />
-        <Generate :type="IconCamera" copy-text="<IconCamera />" />
-        <Generate :type="IconCaretBottom" copy-text="<IconCaretBottom />" />
-        <Generate :type="IconCaretLeft" copy-text="<IconCaretLeft />" />
-        <Generate :type="IconCaretRight" copy-text="<IconCaretRight />" />
-        <Generate :type="IconCaretTop" copy-text="<IconCaretTop />" />
-        <Generate :type="IconCart" copy-text="<IconCart />" />
-        <Generate :type="IconCheckmark" copy-text="<IconCheckmark />" />
-        <Generate :type="IconChevronBottom" copy-text="<IconChevronBottom />" />
-        <Generate :type="IconChevronLeft" copy-text="<IconChevronLeft />" />
-        <Generate :type="IconChevronRight" copy-text="<IconChevronRight />" />
-        <Generate :type="IconChevronTop" copy-text="<IconChevronTop />" />
-        <Generate :type="IconClipboard" copy-text="<IconClipboard />" />
-        <Generate :type="IconClock" copy-text="<IconClock />" />
-        <Generate :type="IconClose" copy-text="<IconClose />" />
-        <Generate :type="IconCode" copy-text="<IconCode />" />
-        <Generate :type="IconCompose" copy-text="<IconCompose />" />
-        <Generate :type="IconCreditcard" copy-text="<IconCreditcard />" />
-        <Generate :type="IconDesktop" copy-text="<IconDesktop />" />
-        <Generate :type="IconDownload" copy-text="<IconDownload />" />
-        <Generate :type="IconEdit" copy-text="<IconEdit />" />
-        <Generate :type="IconEject" copy-text="<IconEject />" />
-        <Generate
-            :type="IconEllipsisHorizontal"
-            copy-text="<IconEllipsisHorizontal />"
-        />
-        <Generate
-            :type="IconEllipsisVertical"
-            copy-text="<IconEllipsisVertical />"
-        />
-        <Generate :type="IconEnd" copy-text="<IconEnd />" />
-        <Generate :type="IconExport" copy-text="<IconExport />" />
-        <Generate :type="IconExternal" copy-text="<IconExternal />" />
-        <Generate :type="IconEye" copy-text="<IconEye />" />
-        <Generate :type="IconFeed" copy-text="<IconFeed />" />
-        <Generate :type="IconFile" copy-text="<IconFile />" />
-        <Generate :type="IconFilter" copy-text="<IconFilter />" />
-        <Generate :type="IconFire" copy-text="<IconFire />" />
-        <Generate :type="IconFlag" copy-text="<IconFlag />" />
-        <Generate :type="IconFolder" copy-text="<IconFolder />" />
-        <Generate :type="IconFolderOpen" copy-text="<IconFolderOpen />" />
-        <Generate :type="IconForwards" copy-text="<IconForwards />" />
-        <Generate :type="IconFullscreen" copy-text="<IconFullscreen />" />
-        <Generate
-            :type="IconFullscreenExit"
-            copy-text="<IconFullscreenExit />"
-        />
-        <Generate :type="IconGift" copy-text="<IconGift />" />
-        <Generate :type="IconGithub" copy-text="<IconGithub />" />
-        <Generate :type="IconHeart" copy-text="<IconHeart />" />
-        <Generate :type="IconHome" copy-text="<IconHome />" />
-        <Generate :type="IconImport" copy-text="<IconImport />" />
-        <Generate :type="IconInbox" copy-text="<IconInbox />" />
-        <Generate :type="IconInfo" copy-text="<IconInfo />" />
-        <Generate :type="IconLightning" copy-text="<IconLightning />" />
-        <Generate :type="IconLink" copy-text="<IconLink />" />
-        <Generate :type="IconLocation" copy-text="<IconLocation />" />
-        <Generate :type="IconLock" copy-text="<IconLock />" />
-        <Generate :type="IconMail" copy-text="<IconMail />" />
-        <Generate :type="IconMenu" copy-text="<IconMenu />" />
-        <Generate :type="IconMessage" copy-text="<IconMessage />" />
-        <Generate :type="IconMicrophone" copy-text="<IconMicrophone />" />
-        <Generate :type="IconMinus" copy-text="<IconMinus />" />
-        <Generate :type="IconMobile" copy-text="<IconMobile />" />
-        <Generate :type="IconMoon" copy-text="<IconMoon />" />
-        <Generate :type="IconMove" copy-text="<IconMove />" />
-        <Generate :type="IconMusic" copy-text="<IconMusic />" />
-        <Generate :type="IconMute" copy-text="<IconMute />" />
-        <Generate :type="IconOptions" copy-text="<IconOptions />" />
-        <Generate :type="IconPaperclip" copy-text="<IconPaperclip />" />
-        <Generate :type="IconPause" copy-text="<IconPause />" />
-        <Generate :type="IconPhoto" copy-text="<IconPhoto />" />
-        <Generate :type="IconPlay" copy-text="<IconPlay />" />
-        <Generate :type="IconPlus" copy-text="<IconPlus />" />
-        <Generate :type="IconPortfolio" copy-text="<IconPortfolio />" />
-        <Generate :type="IconPrint" copy-text="<IconPrint />" />
-        <Generate :type="IconReload" copy-text="<IconReload />" />
-        <Generate :type="IconReply" copy-text="<IconReply />" />
-        <Generate :type="IconSearch" copy-text="<IconSearch />" />
-        <Generate :type="IconSend" copy-text="<IconSend />" />
-        <Generate :type="IconSettings" copy-text="<IconSettings />" />
-        <Generate :type="IconSignIn" copy-text="<IconSignIn />" />
-        <Generate :type="IconSignOut" copy-text="<IconSignOut />" />
-        <Generate :type="IconStar" copy-text="<IconStar />" />
-        <Generate :type="IconStart" copy-text="<IconStart />" />
-        <Generate :type="IconTag" copy-text="<IconTag />" />
-        <Generate :type="IconTelephone" copy-text="<IconTelephone />" />
-        <Generate :type="IconTrash" copy-text="<IconTrash />" />
-        <Generate :type="IconTwitter" copy-text="<IconTwitter />" />
-        <Generate :type="IconUnlock" copy-text="<IconUnlock />" />
-        <Generate :type="IconUpload" copy-text="<IconUpload />" />
-        <Generate :type="IconUser" copy-text="<IconUser />" />
-        <Generate :type="IconVideo" copy-text="<IconVideo />" />
-        <Generate :type="IconVolume" copy-text="<IconVolume />" />
-        <Generate :type="IconWork" copy-text="<IconWork />" />
-        <Generate :type="IconZoomIn" copy-text="<IconZoomIn />" />
-        <Generate :type="IconZoomOut" copy-text="<IconZoomOut />" />
-        <Generate :type="IconZoomReset" copy-text="<IconZoomReset />" />
+        <Generate :type="IconActivity" copy-text="<IconActivity />"/>
+        <Generate :type="IconAlert" copy-text="<IconAlert />"/>
+        <Generate :type="IconArchive" copy-text="<IconArchive />"/>
+        <Generate :type="IconArrowBottom" copy-text="<IconArrowBottom />"/>
+        <Generate :type="IconArrowLeft" copy-text="<IconArrowLeft />"/>
+        <Generate :type="IconArrowRight" copy-text="<IconArrowRight />"/>
+        <Generate :type="IconArrowTop" copy-text="<IconArrowTop />"/>
+        <Generate :type="IconBackwards" copy-text="<IconBackwards />"/>
+        <Generate :type="IconBag" copy-text="<IconBag />"/>
+        <Generate :type="IconBan" copy-text="<IconBan />"/>
+        <Generate :type="IconBell" copy-text="<IconBell />"/>
+        <Generate :type="IconBook" copy-text="<IconBook />"/>
+        <Generate :type="IconBookmark" copy-text="<IconBookmark />"/>
+        <Generate :type="IconCalendar" copy-text="<IconCalendar />"/>
+        <Generate :type="IconCamera" copy-text="<IconCamera />"/>
+        <Generate :type="IconCaretBottom" copy-text="<IconCaretBottom />"/>
+        <Generate :type="IconCaretLeft" copy-text="<IconCaretLeft />"/>
+        <Generate :type="IconCaretRight" copy-text="<IconCaretRight />"/>
+        <Generate :type="IconCaretTop" copy-text="<IconCaretTop />"/>
+        <Generate :type="IconCart" copy-text="<IconCart />"/>
+        <Generate :type="IconCheckmark" copy-text="<IconCheckmark />"/>
+        <Generate :type="IconChevronBottom" copy-text="<IconChevronBottom />"/>
+        <Generate :type="IconChevronLeft" copy-text="<IconChevronLeft />"/>
+        <Generate :type="IconChevronRight" copy-text="<IconChevronRight />"/>
+        <Generate :type="IconChevronTop" copy-text="<IconChevronTop />"/>
+        <Generate :type="IconClipboard" copy-text="<IconClipboard />"/>
+        <Generate :type="IconClock" copy-text="<IconClock />"/>
+        <Generate :type="IconClose" copy-text="<IconClose />"/>
+        <Generate :type="IconCode" copy-text="<IconCode />"/>
+        <Generate :type="IconCompose" copy-text="<IconCompose />"/>
+        <Generate :type="IconCreditcard" copy-text="<IconCreditcard />"/>
+        <Generate :type="IconDesktop" copy-text="<IconDesktop />"/>
+        <Generate :type="IconDownload" copy-text="<IconDownload />"/>
+        <Generate :type="IconEdit" copy-text="<IconEdit />"/>
+        <Generate :type="IconEject" copy-text="<IconEject />"/>
+        <Generate :type="IconEllipsisHorizontal" copy-text="<IconEllipsisHorizontal />"/>
+        <Generate :type="IconEllipsisVertical" copy-text="<IconEllipsisVertical />"/>
+        <Generate :type="IconEnd" copy-text="<IconEnd />"/>
+        <Generate :type="IconExport" copy-text="<IconExport />"/>
+        <Generate :type="IconExternal" copy-text="<IconExternal />"/>
+        <Generate :type="IconEye" copy-text="<IconEye />"/>
+        <Generate :type="IconFeed" copy-text="<IconFeed />"/>
+        <Generate :type="IconFile" copy-text="<IconFile />"/>
+        <Generate :type="IconFilter" copy-text="<IconFilter />"/>
+        <Generate :type="IconFire" copy-text="<IconFire />"/>
+        <Generate :type="IconFlag" copy-text="<IconFlag />"/>
+        <Generate :type="IconFolder" copy-text="<IconFolder />"/>
+        <Generate :type="IconFolderOpen" copy-text="<IconFolderOpen />"/>
+        <Generate :type="IconForwards" copy-text="<IconForwards />"/>
+        <Generate :type="IconFullscreen" copy-text="<IconFullscreen />"/>
+        <Generate :type="IconFullscreenExit" copy-text="<IconFullscreenExit />"/>
+        <Generate :type="IconGift" copy-text="<IconGift />"/>
+        <Generate :type="IconGithub" copy-text="<IconGithub />"/>
+        <Generate :type="IconHeart" copy-text="<IconHeart />"/>
+        <Generate :type="IconHome" copy-text="<IconHome />"/>
+        <Generate :type="IconImport" copy-text="<IconImport />"/>
+        <Generate :type="IconInbox" copy-text="<IconInbox />"/>
+        <Generate :type="IconInfo" copy-text="<IconInfo />"/>
+        <Generate :type="IconLightning" copy-text="<IconLightning />"/>
+        <Generate :type="IconLink" copy-text="<IconLink />"/>
+        <Generate :type="IconLocation" copy-text="<IconLocation />"/>
+        <Generate :type="IconLock" copy-text="<IconLock />"/>
+        <Generate :type="IconMail" copy-text="<IconMail />"/>
+        <Generate :type="IconMenu" copy-text="<IconMenu />"/>
+        <Generate :type="IconMessage" copy-text="<IconMessage />"/>
+        <Generate :type="IconMicrophone" copy-text="<IconMicrophone />"/>
+        <Generate :type="IconMinus" copy-text="<IconMinus />"/>
+        <Generate :type="IconMobile" copy-text="<IconMobile />"/>
+        <Generate :type="IconMoon" copy-text="<IconMoon />"/>
+        <Generate :type="IconMove" copy-text="<IconMove />"/>
+        <Generate :type="IconMusic" copy-text="<IconMusic />"/>
+        <Generate :type="IconMute" copy-text="<IconMute />"/>
+        <Generate :type="IconOptions" copy-text="<IconOptions />"/>
+        <Generate :type="IconPaperclip" copy-text="<IconPaperclip />"/>
+        <Generate :type="IconPause" copy-text="<IconPause />"/>
+        <Generate :type="IconPhoto" copy-text="<IconPhoto />"/>
+        <Generate :type="IconPlay" copy-text="<IconPlay />"/>
+        <Generate :type="IconPlus" copy-text="<IconPlus />"/>
+        <Generate :type="IconPortfolio" copy-text="<IconPortfolio />"/>
+        <Generate :type="IconPrint" copy-text="<IconPrint />"/>
+        <Generate :type="IconReload" copy-text="<IconReload />"/>
+        <Generate :type="IconReply" copy-text="<IconReply />"/>
+        <Generate :type="IconSearch" copy-text="<IconSearch />"/>
+        <Generate :type="IconSend" copy-text="<IconSend />"/>
+        <Generate :type="IconSettings" copy-text="<IconSettings />"/>
+        <Generate :type="IconSignIn" copy-text="<IconSignIn />"/>
+        <Generate :type="IconSignOut" copy-text="<IconSignOut />"/>
+        <Generate :type="IconStar" copy-text="<IconStar />"/>
+        <Generate :type="IconStart" copy-text="<IconStart />"/>
+        <Generate :type="IconTag" copy-text="<IconTag />"/>
+        <Generate :type="IconTelephone" copy-text="<IconTelephone />"/>
+        <Generate :type="IconTrash" copy-text="<IconTrash />"/>
+        <Generate :type="IconTwitter" copy-text="<IconTwitter />"/>
+        <Generate :type="IconUnlock" copy-text="<IconUnlock />"/>
+        <Generate :type="IconUpload" copy-text="<IconUpload />"/>
+        <Generate :type="IconUser" copy-text="<IconUser />"/>
+        <Generate :type="IconVideo" copy-text="<IconVideo />"/>
+        <Generate :type="IconVolume" copy-text="<IconVolume />"/>
+        <Generate :type="IconWork" copy-text="<IconWork />"/>
+        <Generate :type="IconZoomIn" copy-text="<IconZoomIn />"/>
+        <Generate :type="IconZoomOut" copy-text="<IconZoomOut />"/>
+        <Generate :type="IconZoomReset" copy-text="<IconZoomReset />"/>
     </div>
 </template>
 
 <script setup>
-import Generate from './generate.jsx';
+import Generate from './generate.jsx'
 import {
     IconActivity,
     IconAlert,
@@ -218,27 +209,31 @@ import {
     IconWork,
     IconZoomIn,
     IconZoomOut,
-    IconZoomReset,
-} from 'ungeicons';
+    IconZoomReset
+} from 'ungeicons'
 </script>
 
 <style>
-.u-icons {
-    font-size: 50px;
-    display: flex;
-    flex-wrap: wrap;
-}
-.u-icons > i {
-    margin: 10px;
-    padding: 10px;
-    line-height: 1;
-    transition: all 0.2s;
-}
-.u-icons > i:hover {
-    cursor: pointer;
-    transition: all 0.2s;
-    border-radius: 4px;
-    transform: translateY(-5px);
-    box-shadow: 0 0 4px #aaa;
-}
+    .u-icons {
+        font-size: 50px;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(87px, 1fr))
+        /* display: flex; */
+        /* flex-wrap: wrap; */
+    }
+    .u-icons > i {
+        /* width: 5%; */
+        text-align: center;
+        margin: 10px;
+        padding: 10px;
+        line-height: 1;
+        transition: all .2s;
+    }
+    .u-icons > i:hover {
+        cursor: pointer;
+        transition: all .2s;
+        border-radius: 4px;
+        transform: translateY(-5px);
+        box-shadow: 0 0 4px #aaa;
+    }
 </style>
