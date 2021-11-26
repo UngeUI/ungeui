ce4e50514b8847394f0334d34e3f0bb50a32e88d
Peter roe <peterroe@163.com>
2021-11-26 12:24:27 +0800

    :sparkles: feat(space): docs update

--- src/space/demo/grap.vue
+++ src/space/demo/grap.vue
@@ -1,6 +1,6 @@
 <template>
    <div>
-        <u-input-number v-model:value="value"></u-input-number>
+        <u-input-number :step="20" v-model:value="value"></u-input-number>
         <u-space :grap="value">
             <u-button>按钮</u-button>
             <u-button>按钮</u-button>
