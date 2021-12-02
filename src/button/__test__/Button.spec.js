236b7fc415d03d964f077215e9dbfcf8c2a6c921
Peter roe <peterroe@163.com>
2021-12-02 22:33:15 +0800

    :white_check_mark: test: update

--- src/button/__test__/Button.spec.js
+++ src/button/__test__/Button.spec.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { h } from 'vue'
-import UButton from '../button.jsx'
+import UButton from '../index.js'
 import UIcon from '../../icon/index'
 import { IconMessage } from '@ungeui/icons'
 
@@ -9,7 +9,7 @@ describe('u-button', () => {
         mount(UButton)
     })
 
-    it('clickable', async () => {
+    it('clickable', async() => {
         const onClick = jest.fn()
         const wrapper = mount(UButton, {
             props: {
@@ -21,7 +21,7 @@ describe('u-button', () => {
         expect(onClick).toHaveBeenCalled()
     })
 
-    it('disabled', async () => {
+    it('disabled', async() => {
         const onClick = jest.fn()
         mount(UButton, {
             props: {
@@ -32,12 +32,12 @@ describe('u-button', () => {
         expect(onClick).not.toHaveBeenCalled()
     })
 
-    it('should work with `type` prop', async () => {
-        ['default','success','danger','warning','info'].forEach(type => {
+    it('should work with `type` prop', async() => {
+        ['default', 'success', 'danger', 'warning', 'info'].forEach(type => {
             const wrapper = mount(UButton, {
-               props:{
-                   type
-               }
+                props: {
+                    type
+                }
             })
             expect(wrapper.find('.u-button').classes()).toContain(
                 `u-button-${type}`
@@ -46,9 +46,9 @@ describe('u-button', () => {
         })
     })
 
-    it('should work with `deep` prop', async () => {
+    it('should work with `deep` prop', async() => {
         const wrapper = mount(UButton, {
-            props:{
+            props: {
                 deep: true
             }
         })
@@ -58,12 +58,12 @@ describe('u-button', () => {
         wrapper.unmount()
     })
 
-    it('should work with `size` props', async () => {
-        ['small','medium','large','huge'].forEach(size => {
+    it('should work with `size` props', async() => {
+        ['small', 'medium', 'large', 'huge'].forEach(size => {
             const wrapper = mount(UButton, {
-               props:{
-                   size
-               }
+                props: {
+                    size
+                }
             })
             expect(wrapper.find('.u-button').classes()).toContain(
                 `u-button-size-${size}`
@@ -72,7 +72,7 @@ describe('u-button', () => {
         })
     })
 
-    it('should work with `text` props', async () => {
+    it('should work with `text` props', async() => {
         const wrapper = mount(UButton, {
             props: {
                 text: true
@@ -83,33 +83,31 @@ describe('u-button', () => {
         )
     })
 
-    it('should work with `prefix` slot', async () => {
+    it('should work with `prefix` slot', async() => {
         const wrapper = mount(UButton, {
             slots: {
-                prefix:() => h(
-                    UIcon, 
-                    null,
-                    {default: () => h(IconMessage)}
+                prefix: () => h(
+                    UIcon,
+                    null, { default: () => h(IconMessage) }
                 )
             }
         })
         expect(wrapper.find('i').classes('u-icon')).toBe(true)
     })
 
-    it('should work with `suffix` slot', async () => {
+    it('should work with `suffix` slot', async() => {
         const wrapper = mount(UButton, {
             slots: {
-                suffix:() => h(
-                    UIcon, 
-                    null,
-                    {default: () => h(IconMessage)}
+                suffix: () => h(
+                    UIcon,
+                    null, { default: () => h(IconMessage) }
                 )
             }
         })
         expect(wrapper.find('i').classes('u-icon')).toBe(true)
     })
 
-    it('should work with `round`', async () => {
+    it('should work with `round`', async() => {
         const wrapper = mount(UButton, {
             props: {
                 round: true
@@ -117,8 +115,7 @@ describe('u-button', () => {
             slots: {
                 prefix: () => h(
                     UIcon,
-                    null,
-                    {default: () => h(IconMessage)}
+                    null, { default: () => h(IconMessage) }
                 )
             }
         })
