5397af66aa2b859a651ca9f11601eca324ab3699
Peter roe <peterroe@163.com>
2021-12-17 20:44:11 +0800

    :tada: feat: demo test

---
+++ demo/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+let data = ref([{
+    title: 'parent 1',
+    key: '0-0',
+    children: [
+		{
+			title: 'parent 1-0',
+			key: '0-0-0',
+			disabled: true,
+			children: [
+				{ 
+					title: 'leaf', 
+					key: '0-0-0-0', 
+					disableCheckbox: true 
+				},
+				{ 
+					title: 'leaf', 
+					key: '0-0-0-1' 
+				},
+			],
+		},
+		{
+			title: 'parent 1-1',
+			key: '0-0-1',
+			children: [
+				{ 
+					key: '0-0-1-0', 
+					title: 'title0010'
+				}
+			],
+		},
+	],
+}])
+</script>
+
+<template>
+  <u-tree :data="data">d</u-tree>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Ungeui from '../../dist/ungeui.es'
+import '../../dist/style.css'
+createApp(App).use(Ungeui).mount('#app')
